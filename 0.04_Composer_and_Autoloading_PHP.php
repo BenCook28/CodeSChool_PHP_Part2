941ed6d6417bdb6684fb9b40d38eb07ab783ba06
Ben Cook <benthomascook28@gmail.com>
2017-09-17 21:48:46 -0400

    Added Composer-setup and package info

--- 0.04_Composer_and_Autoloading_PHP.php
+++ 0.04_Composer_and_Autoloading_PHP.php
@@ -3,3 +3,9 @@ side effects, but not both. -->
 <?php
 __DIR__ //is a magic constant in PHP which gives us 
 //an absolute path of the current file.
+
+// composer require respect/validation
+
+//use Respect/Validation/Validator; to use things in package
+
+// $v = new Validator; creating a new object based on Validator class
